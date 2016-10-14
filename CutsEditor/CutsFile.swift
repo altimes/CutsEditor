005bf1a070a37fced2baa7d942879e14ccd4f93d
Alan Franklin <alafrankli@gmail.com>
2016-10-14 11:40:37 +1100

    Changes to support Xcode 8, swift 3. Floating Player controls, tracking and display of current playback position.  Abstraction of cuts info into CutsFile class

--- CutsEditor/CutsFile.swift
+++ CutsEditor/CutsFile.swift
@@ -16,9 +16,26 @@
 import Cocoa
 import AVFoundation
 
+struct MessageStrings {
+  static let NO_SUCH_FILE = "No Such File"
+  static let FOUND_FILE = "Found File"
+  static let CAN_CREATE_FILE = "Success on file creation"
+  static let DID_WRITE_FILE = "Success of replacement"
+}
+
+
 open class CutsFile: NSObject {
   
   // model: array of cuts data
+  
+  fileprivate enum cutStates {
+    case
+      unknown,
+      inCut,
+      outCut,
+      bookmark,
+      lastplay
+  }
   
   var cutsArray = [CutEntry]()
   open var count : Int
@@ -41,7 +58,62 @@ open class CutsFile: NSObject {
     }
   }
   
+  open var first: CutEntry? {
+    get {
+      return cutsArray.first
+    }
+  }
+  
+  open var last: CutEntry? {
+    get {
+      return cutsArray.last
+    }
+  }
+  
+  
+  /// Get the last OUT mark
+  open var lastOutCutMark: CutEntry? {
+    get {
+      let OUTArray = cutsArray.filter() {$0.cutType == MARK_TYPE.OUT.rawValue}
+      return OUTArray.last
+    }
+  }
+  
+  /// Get the first IN mark
+  open var firstInCutMark: CutEntry? {
+    get {
+      let INArray = cutsArray.filter() {$0.cutType == MARK_TYPE.IN.rawValue}
+      return INArray.first
+    }
+  }
+  
+  /// Get the first OUT mark
+  open var firstOutCutMark: CutEntry? {
+    get {
+      let OUTArray = cutsArray.filter() {$0.cutType == MARK_TYPE.OUT.rawValue}
+      return OUTArray.first
+    }
+  }
+  
+  var isCuttable : Bool {
+    get {
+      let validation = self.validateInOut()
+      return validation.result
+    }
+  }
+  
+  var isModified : Bool {
+     get {
+      return modified
+    }
+  }
+  
+  fileprivate var modified = false
+  
+  
   var debug = false
+  
+  public static let marksDictionary = ["addBookmark":MARK_TYPE.BOOKMARK,"addInMark":MARK_TYPE.IN, "addOutMark":MARK_TYPE.OUT, "addLastPlay":MARK_TYPE.LASTPLAY]
   
   // MARK: Initializers
   
@@ -50,11 +122,168 @@ open class CutsFile: NSObject {
     self.init()
     decodeCutsData(data)
   }
+  
   // supporting functions
   
   open static func makeCutEntry(_ cutPts : UInt64, type : MARK_TYPE) -> CutEntry
   {
     return CutEntry(cutPts: cutPts, cutType: type.rawValue)
+  }
+  
+  open func entry(at index: Int) -> CutEntry?
+  {
+    // validate index
+    if (index >= 0 && index < cutsArray.count) {
+      return cutsArray[index]
+    }
+    else {
+      return nil
+    }
+  }
+  
+  open func contains(_ entry: CutEntry) -> Bool
+  {
+    return cutsArray.contains(entry)
+  }
+  
+  /// Add cut entry to array
+  /// FIXME: should return SUCCESS or FAILURE ?
+  func addEntry(_ cutEntry: CutEntry) {
+    // check if already present
+    if (!cutsArray.contains(cutEntry)) {
+      insert(cutEntry)
+    }
+  }
+  
+  /// Append cutEntry to the cuts Storage and
+  /// resort the storage.
+  /// Maintains storage in ascending order
+  open func insert(_ entry: CutEntry)
+  {
+    modified = true
+    cutsArray.append(entry)
+    cutsArray.sort(by: <)
+  }
+
+  
+  /// Add bookmarks to the array at a fixed time interval.
+  /// Early simplistic implementation to caculate PTS value from first
+  /// PTS value.  Later may interogate .ap file for nearest PTS value.
+  /// Uses preference value for time interval
+  /// Note that this deliberately avoids a begin boundary bookmark
+  /// Cutting occurs on GOP boundaries and can end up with negative
+  /// bookmarks.
+  
+  func addFixedTimeBookmarks (interval: Int, firstInCutPts: PtsType, lastOutCutPts: PtsType)
+  {
+    
+    let intervalInSeconds = interval
+    let ptsIncrement = PtsType( intervalInSeconds*Int(CutsTimeConst.PTS_TIMESCALE))
+    // get duration
+    if (firstInCutPts < lastOutCutPts)  // important for UIntXX values
+    {
+      var pts = firstInCutPts + ptsIncrement
+      while pts < lastOutCutPts
+      {
+        let cutPt = CutEntry(cutPts: pts, cutType: MARK_TYPE.BOOKMARK.rawValue)
+        addEntry(cutPt)
+        pts += ptsIncrement
+      }
+      modified = true
+    }
+  }
+
+  /// Note that this deliberately avoids a begin boundary bookmark
+  /// Cutting occurs on GOP boundaries and can end up with negative
+  /// bookmarks.
+  /// Default value give bookmarks at 10 % boundaries
+  
+  func addPercentageBookMarks(_ numberOfMarks: Int = 9, firstInCutPts: PtsType, lastOutCutPts: PtsType)
+  {
+    var countAdded = 0
+    // get duration
+    if (firstInCutPts < lastOutCutPts)  // important for UIntXX values
+    {
+      let programLength = lastOutCutPts - firstInCutPts
+      let ptsOffset = programLength / UInt64(numberOfMarks+1)
+      var ptsPosition = firstInCutPts + ptsOffset
+      while (ptsPosition < lastOutCutPts && countAdded < numberOfMarks)
+      {
+        //        print("\(pts)  - \(offset)")
+        let cutPt = CutEntry(cutPts: ptsPosition, cutType: MARK_TYPE.BOOKMARK.rawValue)
+        addEntry(cutPt)
+        ptsPosition += ptsOffset
+        countAdded += 1
+      }
+      modified = true
+    }
+  }
+  
+
+  /// Remove the given cutEntry from the cuts storage.
+  /// Silently ignor missing entry.
+  /// - parameter cutEntry: entry structure to remove
+  open func removeEntry(_ cutEntry: CutEntry) -> Bool
+  {
+    guard let index = cutsArray.index(of: cutEntry) else {
+      return false
+    }
+    cutsArray.remove(at: index)
+    modified = true
+    return true
+  }
+  
+  /// Wrapper function to abstract implementation
+  open func removeAll() {
+    cutsArray.removeAll()
+    modified = true
+  }
+
+  /// Wrapper function
+  open func remove(at index: Int)
+  {
+    guard (cutsArray.count > 0 && index>=0 && index < cutsArray.count) else { return }
+    cutsArray.remove(at: index)
+    modified = true
+  }
+  
+  open func removeEntriesOfType(_ type: MARK_TYPE)
+  {
+    // replace with array with all marks except "type"
+    cutsArray = cutsArray.filter() {!($0.cutType == type.rawValue)}
+    modified = true
+  }
+  
+  open func index(of entry: CutEntry) -> Int?
+  {
+    return cutsArray.index(of: entry)
+  }
+
+  /// Find the cut entry that preceeds the current time or is within the provivded tolerance of the
+  /// time.  That is, if time is 15.99, return the 16.0 cut entry
+  open func entryBeforeTime(_ secs: Double, tolerance: Double = 0.05) -> (entry :CutEntry, index: Int)?
+  {
+    var found: CutEntry? = nil
+    for entry in cutsArray {
+      let entrySecs = entry.asSeconds()
+      if ((entrySecs - secs) <= tolerance) {
+        found = entry
+      }
+      else {  // now is less than this mark
+        if let mark = found
+        { // there was a previous mark
+          return (mark, cutsArray.index(of: mark)!)
+        }
+        else {
+          break
+        }
+      }
+    }
+    if let mark = found
+    { // there was a previous mark
+      return (mark, cutsArray.index(of: mark)!)
+    }
+    return nil
   }
   
   // core accessors decode/encode
@@ -103,6 +332,44 @@ open class CutsFile: NSObject {
 //    }
 //    return success
 //  }
+
+  
+  /// Routine that looks at the program time and decide if it is in
+  /// a "cut me out" section and if so, return the next IN time or nil
+  open func programTimeAfter(_ now: CMTime) -> CMTime?
+  {
+    var skipCandidate = false
+    let nowInSecs = now.seconds
+    for entry in cutsArray {
+      var markInSecs = entry.asSeconds()
+      if (entry.cutType == MARK_TYPE.IN.rawValue || entry.cutType == MARK_TYPE.OUT.rawValue)
+      {
+//        let type = MARK_TYPE(rawValue: entry.cutType)!
+//        print("seeing now as \(nowInSecs) and mark as \(markInSecs) with mark \(type.description()) and candidate = \(skipCandidate)")
+        var markTime = CMTimeMake(Int64(entry.cutPts), CutsTimeConst.PTS_TIMESCALE)
+        
+        if (nowInSecs > markInSecs && entry.cutType == MARK_TYPE.OUT.rawValue)
+        {
+          // skip ad candidate
+          skipCandidate = true
+          continue
+        }
+        
+        if (nowInSecs < markInSecs && entry.cutType == MARK_TYPE.IN.rawValue && skipCandidate) {
+//          print (" comp \(nowInSecs) vs \(markInSecs)")
+//          print ("\(markTime) - \(now)")
+          markInSecs += 0.25
+          markTime = CMTimeMake(Int64(markInSecs*1000.0)*Int64(CutsTimeConst.PTS_TIMESCALE/1000), CutsTimeConst.PTS_TIMESCALE)
+//          markTime = CMTimeConvertScale(markTime, now.timescale, .roundAwayFromZero)
+          return markTime
+        }
+        else {
+          skipCandidate = false
+        }
+      }
+    }
+    return nil
+  }
   
   open func decodeCutsData(_ data: Data)
   {
@@ -127,6 +394,7 @@ open class CutsFile: NSObject {
         cutsArray[i].cutType = UInt32(bigEndian: tempCutEntry.cutType)
       }
       cutsArray.sort(by: <)
+      modified = false
   }
   
 //  public func loadCutsDataFromFile(name:String)
@@ -202,11 +470,11 @@ open class CutsFile: NSObject {
     return found
   }
   
-  func firstINMark() -> CutEntry?
-  {
-    let INArray = cutsArray.filter() {$0.cutType == MARK_TYPE.IN.rawValue}
-    return INArray.first
-  }
+//  func firstINMark() -> CutEntry?
+//  {
+//    let INArray = cutsArray.filter() {$0.cutType == MARK_TYPE.IN.rawValue}
+//    return INArray.first
+//  }
   
 //  public func appendEntry(item : CutEntry)
 //  {
@@ -289,14 +557,47 @@ open class CutsFile: NSObject {
     if (fileWritten) {
       print(MessageStrings.DID_WRITE_FILE)
     }
+    modified = false
   }
   
-  // - check all pts within range of corresponding file
-  // - check if at least one IN OUT pair exist - if not, then fabricate as
-  //   first or last file point
+  /// check that there are no IN, IN or OUT, OUT sequences present
+  /// - returns: result of validation and message if flaw was found or empty string if good
   
-//  public func validate() -> Bool
-//  {
-//    return false
-//  }
+  open func validateInOut() -> (result: Bool, errorMessage: String) {
+    // check in/out pairing
+    var currentState = cutStates.unknown
+    var goodList = true
+    var message = ""
+    for item in cutsArray
+    {
+      switch MARK_TYPE(rawValue: item.cutType)! {
+      case .IN :
+        if (currentState == .unknown || currentState == .outCut) {
+          currentState = .inCut
+          goodList = goodList && true
+        }
+        else {
+          goodList = false
+          message = "Sequential IN cut Marks"
+          break
+        }
+      case .OUT :
+        if (currentState == .unknown || currentState == .inCut ) {
+          currentState = .outCut
+          goodList = goodList && true
+        }
+        else {
+          goodList = false
+          message = "Sequential OUT cut Marks"
+          break
+        }
+      case .BOOKMARK : fallthrough
+      case .LASTPLAY :
+        // does not change state
+        goodList = goodList && true
+      }
+    }
+    
+    return (goodList, message)
+  }
 }
